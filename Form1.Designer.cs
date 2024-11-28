91520c709387f7874ca6b655ab407b9c6b6bad1b
M.I.C.C.C <davidforce024@gmail.com>
2024-11-28 12:14:00 +0330

    Basic body is redy

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,29 +33,40 @@
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            customProgressBar1 = new CustomProgressBar();
-            chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            ((System.ComponentModel.ISupportInitialize)chart2).BeginInit();
+            tempProgressBar = new CustomProgressBar();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            label1 = new Label();
+            label2 = new Label();
+            txtCpuLoad = new Label();
+            txtCpuTemp = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            trackBar1 = new TrackBar();
+            trackBar2 = new TrackBar();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
             SuspendLayout();
             // 
-            // customProgressBar1
+            // tempProgressBar
             // 
-            customProgressBar1.BackColor = Color.DimGray;
-            customProgressBar1.ForeColor = Color.Gainsboro;
-            customProgressBar1.Location = new Point(73, 374);
-            customProgressBar1.Name = "customProgressBar1";
-            customProgressBar1.Size = new Size(257, 23);
-            customProgressBar1.TabIndex = 0;
-            customProgressBar1.Value = 100;
+            tempProgressBar.BackColor = Color.DimGray;
+            tempProgressBar.ForeColor = Color.Gainsboro;
+            tempProgressBar.Location = new Point(0, 136);
+            tempProgressBar.Name = "tempProgressBar";
+            tempProgressBar.Size = new Size(800, 23);
+            tempProgressBar.Style = ProgressBarStyle.Continuous;
+            tempProgressBar.TabIndex = 0;
+            tempProgressBar.Value = 100;
             // 
-            // chart2
+            // chart1
             // 
             chartArea1.Name = "ChartArea1";
-            chart2.ChartAreas.Add(chartArea1);
+            chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
-            chart2.Legends.Add(legend1);
-            chart2.Location = new Point(0, 1);
-            chart2.Name = "chart2";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(0, 1);
+            chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.Legend = "Legend1";
@@ -64,37 +75,139 @@
             series2.ChartArea = "ChartArea1";
             series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series2.Legend = "Legend1";
-            series2.LegendText = "CPU Temprature";
+            series2.LegendText = "CPU Temperature";
             series2.Name = "CpuTemp";
             series3.ChartArea = "ChartArea1";
             series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series3.Legend = "Legend1";
             series3.LegendText = "Fan Speed";
             series3.Name = "FanSpeed";
-            chart2.Series.Add(series1);
-            chart2.Series.Add(series2);
-            chart2.Series.Add(series3);
-            chart2.Size = new Size(800, 129);
-            chart2.TabIndex = 1;
-            chart2.Text = "THE CHART!";
+            chart1.Series.Add(series1);
+            chart1.Series.Add(series2);
+            chart1.Series.Add(series3);
+            chart1.Size = new Size(800, 129);
+            chart1.TabIndex = 1;
+            chart1.Text = "THE CHART!";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Enabled = false;
+            label1.Location = new Point(52, 162);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 2;
+            label1.Text = "CPU Load:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Enabled = false;
+            label2.Location = new Point(12, 177);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 15);
+            label2.TabIndex = 2;
+            label2.Text = "CPU Temperature:";
+            // 
+            // txtCpuLoad
+            // 
+            txtCpuLoad.AutoSize = true;
+            txtCpuLoad.Enabled = false;
+            txtCpuLoad.Location = new Point(114, 162);
+            txtCpuLoad.Name = "txtCpuLoad";
+            txtCpuLoad.Size = new Size(28, 15);
+            txtCpuLoad.TabIndex = 2;
+            txtCpuLoad.Text = "00.0";
+            txtCpuLoad.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // txtCpuTemp
+            // 
+            txtCpuTemp.AutoSize = true;
+            txtCpuTemp.Enabled = false;
+            txtCpuTemp.Location = new Point(114, 177);
+            txtCpuTemp.Name = "txtCpuTemp";
+            txtCpuTemp.Size = new Size(28, 15);
+            txtCpuTemp.TabIndex = 2;
+            txtCpuTemp.Text = "00.0";
+            txtCpuTemp.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Enabled = false;
+            label5.Location = new Point(138, 177);
+            label5.Name = "label5";
+            label5.Size = new Size(19, 15);
+            label5.TabIndex = 2;
+            label5.Text = "℃";
+            label5.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            label6.Enabled = false;
+            label6.Location = new Point(138, 162);
+            label6.Name = "label6";
+            label6.Size = new Size(88, 15);
+            label6.TabIndex = 2;
+            label6.Text = "%";
+            label6.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(585, 165);
+            trackBar1.Maximum = 20;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(203, 45);
+            trackBar1.TabIndex = 3;
+            trackBar1.Value = 2;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // trackBar2
+            // 
+            trackBar2.Location = new Point(12, 195);
+            trackBar2.Maximum = 50;
+            trackBar2.Minimum = -70;
+            trackBar2.Name = "trackBar2";
+            trackBar2.Size = new Size(776, 45);
+            trackBar2.TabIndex = 3;
+            trackBar2.Scroll += trackBar2_Scroll;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(chart2);
-            Controls.Add(customProgressBar1);
+            Controls.Add(trackBar2);
+            Controls.Add(trackBar1);
+            Controls.Add(label2);
+            Controls.Add(label5);
+            Controls.Add(txtCpuTemp);
+            Controls.Add(label6);
+            Controls.Add(txtCpuLoad);
+            Controls.Add(label1);
+            Controls.Add(chart1);
+            Controls.Add(tempProgressBar);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)chart2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private CustomProgressBar customProgressBar1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private CustomProgressBar tempProgressBar;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private Label label1;
+        private Label label2;
+        private Label txtCpuLoad;
+        private Label txtCpuTemp;
+        private Label label5;
+        private Label label6;
+        private TrackBar trackBar1;
+        private TrackBar trackBar2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -8,8 +8,19 @@ namespace HeatChecker
     {
         private LOHM.Computer _computer;
         private System.Windows.Forms.Timer _timer;
-        private CustomProgressBar tempProgressBar;
-        private Chart chart1;
+        //private Chart chart1;
+
+        private static string SfxDir = Application.StartupPath + @"SFX\";
+        private static string SfxBruh = SfxDir + "bruh.wav";
+        private static string SfxVine = SfxDir + "vine.wav";
+
+        private static string SfxAlarmS = SfxDir + @"alarms\alarm - slow.wav";
+        private static string SfxAlarmM = SfxDir + @"alarms\alarm - medium.wav";
+        private static string SfxAlarmF = SfxDir + @"alarms\alarm - fast.wav";
+        private static string SfxAlarmC = SfxDir + @"alarms\alarm - critical.wav";
+
+        private int M_GLOBAL_INT = 0;
+        private int M_INCRES_INT = 0;
 
         public Form1()
         {
@@ -22,7 +33,7 @@ namespace HeatChecker
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            new System.Media.SoundPlayer(SfxVine).Play();
         }
 
         private void InitializeHardwareMonitor()
@@ -41,25 +52,27 @@ namespace HeatChecker
 
         private void InitializeChart()
         {
-            // Create and configure the Chart
+            /* Create and configure the Chart
             chart1 = new Chart
             {
                 Dock = DockStyle.Top,
                 Height = 300
             };
             this.Controls.Add(chart1);
+            
 
             ChartArea chartArea = new ChartArea();
             chart1.ChartAreas.Add(chartArea);
 
             // Add series for Temperature, CPU Load, and Fan Speed
-            chart1.Series.Add("Temperature");
-            chart1.Series.Add("CPU Load");
-            chart1.Series.Add("Fan Speed");
+            chart1.Series.Add("CpuTemp");
+            chart1.Series.Add("CpuLoad");
+            chart1.Series.Add("FanSpeed");
+            */
 
-            chart1.Series["Temperature"].ChartType = SeriesChartType.Line;
-            chart1.Series["CPU Load"].ChartType = SeriesChartType.Line;
-            chart1.Series["Fan Speed"].ChartType = SeriesChartType.Line;
+            chart1.Series["CpuTemp"].ChartType = SeriesChartType.Line;
+            chart1.Series["CpuLoad"].ChartType = SeriesChartType.Line;
+            chart1.Series["FanSpeed"].ChartType = SeriesChartType.Spline;
 
             chart1.ChartAreas[0].AxisX.Title = "Time";
             chart1.ChartAreas[0].AxisY.Title = "Value";
@@ -82,44 +95,109 @@ namespace HeatChecker
             {
                 if (hardwareItem.HardwareType == HardwareType.Cpu)
                 {
-                    hardwareItem.Update();
-
                     // Monitor temperature
                     foreach (var sensor in hardwareItem.Sensors)
                     {
-                        if (sensor.SensorType == SensorType.Temperature)
+                        if (sensor.SensorType == SensorType.Temperature && sensor.Name == "Core Average")
                         {
-                            float temp = sensor.Value.GetValueOrDefault();
-                            tempProgressBar.Value = (int)temp; // Update progress bar
-                            chart1.Series["Temperature"].Points.AddY(temp); // Update chart
-                            if (chart1.Series["Temperature"].Points.Count > 100)
-                                chart1.Series["Temperature"].Points.RemoveAt(0);
+                            float temp = sensor.Value.GetValueOrDefault() + M_INCRES_INT;
+                            if (temp < 0)
+                                tempProgressBar.Value = 0;
+                            else if (temp > 100)
+                                tempProgressBar.Value = 100;
+                            else
+                                tempProgressBar.Value = (int)temp; // Update progress bar
+
+                            chart1.Series["CpuTemp"].Points.AddXY(M_GLOBAL_INT, temp); // Update chart
+                            if (chart1.Series["CpuTemp"].Points.Count > 100)
+                                chart1.Series["CpuTemp"].Points.RemoveAt(0);
+
+                            txtCpuTemp.Text = temp.ToString();
+
+                            var category = CategoryBasedOnHeat(temp);
+                            if (category != HeatCategory.Normal)
+                            {
+                                HandleCpuTemprature(category);
+                                continue;
+                            }
+                            txtCpuTemp.Enabled = false;
                         }
                     }
 
                     // Monitor CPU load
                     foreach (var sensor in hardwareItem.Sensors)
                     {
-                        if (sensor.SensorType == SensorType.Load)
+                        if (sensor.SensorType == SensorType.Load && sensor.Name == "CPU Total")
                         {
-                            chart1.Series["CPU Load"].Points.AddY(sensor.Value.GetValueOrDefault());
-                            if (chart1.Series["CPU Load"].Points.Count > 100)
-                                chart1.Series["CPU Load"].Points.RemoveAt(0);
+                            chart1.Series["CpuLoad"].Points.AddXY(M_GLOBAL_INT, sensor.Value.GetValueOrDefault());
+                            if (chart1.Series["CpuLoad"].Points.Count > 100)
+                                chart1.Series["CpuLoad"].Points.RemoveAt(0);
+                            txtCpuLoad.Text = sensor.Value.ToString();
                         }
                     }
-
+                    /*
                     // Monitor fan speed
                     foreach (var sensor in hardwareItem.Sensors)
                     {
                         if (sensor.SensorType == SensorType.Fan)
                         {
-                            chart1.Series["Fan Speed"].Points.AddY(sensor.Value.GetValueOrDefault());
-                            if (chart1.Series["Fan Speed"].Points.Count > 100)
-                                chart1.Series["Fan Speed"].Points.RemoveAt(0);
+                            chart1.Series["FanSpeed"].Points.AddXY(M_GLOBAL_INT, sensor.Value.GetValueOrDefault());
+                            if (chart1.Series["FanSpeed"].Points.Count > 100)
+                                chart1.Series["FanSpeed"].Points.RemoveAt(0);
                         }
                     }
+                    */
+                    hardwareItem.Update();
+                    M_GLOBAL_INT++;
                 }
             }
+        }
+
+        private void HandleCpuTemprature(HeatCategory category)
+        {
+            txtCpuTemp.Enabled = true;
+
+            switch (category)
+            {
+                case HeatCategory.none:
+                    txtCpuTemp.Text = "??.?";
+                    txtCpuTemp.Enabled = false;
+                    break;
+                case HeatCategory.Freezing:
+                    txtCpuTemp.ForeColor = Color.DeepSkyBlue;
+                    break;
+                case HeatCategory.Normal:
+
+                    txtCpuTemp.Enabled = false;
+                    break;
+                case HeatCategory.Warm:
+                    txtCpuTemp.Enabled = false;
+                    break;
+                case HeatCategory.Hot:
+                    txtCpuTemp.ForeColor = Color.Red;
+                    break;
+                case HeatCategory.Critical:
+                    txtCpuTemp.ForeColor = Color.DarkRed;
+                    break;
+                default:
+                    txtCpuTemp.Enabled = false;
+                    break;
+            }
+        }
+
+        private HeatCategory CategoryBasedOnHeat(float? heat)
+        {
+            if (heat <= 0.0f)
+                return HeatCategory.Freezing;
+            if (heat <= 60)
+                return HeatCategory.Normal;
+            if (heat <= 80)
+                return HeatCategory.Warm;
+            if (heat <= 90)
+                return HeatCategory.Hot;
+            if (heat > 90)
+                return HeatCategory.Critical;
+            return HeatCategory.none;
         }
 
         protected override void OnFormClosed(FormClosedEventArgs e)
@@ -130,15 +208,37 @@ namespace HeatChecker
 
         private void InitializeCustomProgressBar()
         {
+            /*
             tempProgressBar = new CustomProgressBar
             {
                 Maximum = 100, // Set maximum temperature (Celsius)
-                Location = new System.Drawing.Point(10, 350),
-                Size = new System.Drawing.Size(300, 30)
+                //Location = new System.Drawing.Point(10, 350),
+                //Size = new System.Drawing.Size(300, 30)
             };
             this.Controls.Add(tempProgressBar);
+            */
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            M_INCRES_INT = trackBar2.Value;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            _timer.Interval = trackBar1.Value * 500 + 1;
         }
     }
+}
+
+enum HeatCategory
+{
+    none,
+    Freezing,
+    Normal,
+    Warm,
+    Hot,
+    Critical
 }
 
 public class CustomProgressBar : Control
