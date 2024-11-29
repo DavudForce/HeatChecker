c7315036480630aacde2984303d75bb18b8e01c8
M.I.C.C.C <davidforce024@gmail.com>
2024-11-29 11:52:04 +0330

    before fixing batery low sound corrumption error.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,9 +43,27 @@
             label6 = new Label();
             trackBar1 = new TrackBar();
             trackBar2 = new TrackBar();
+            label3 = new Label();
+            txtFanSpeed = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            chckPlaySfx = new CheckBox();
+            label14 = new Label();
+            label13 = new Label();
+            label4 = new Label();
+            label15 = new Label();
+            lblBatteryStatus = new Label();
             ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // tempProgressBar
@@ -61,19 +79,24 @@
             // 
             // chart1
             // 
+            chartArea1.AxisX.MaximumAutoSize = 20F;
             chartArea1.Name = "ChartArea1";
             chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
+            legend1.TitleSeparator = System.Windows.Forms.DataVisualization.Charting.LegendSeparatorStyle.ThickLine;
             chart1.Legends.Add(legend1);
             chart1.Location = new Point(0, 1);
             chart1.Name = "chart1";
+            series1.BorderWidth = 2;
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.Legend = "Legend1";
             series1.LegendText = "CPU Load";
+            series1.MarkerBorderWidth = 2;
             series1.Name = "CpuLoad";
+            series2.BorderWidth = 2;
             series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series2.Legend = "Legend1";
             series2.LegendText = "CPU Temperature";
             series2.Name = "CpuTemp";
@@ -154,7 +177,7 @@
             // 
             // trackBar1
             // 
-            trackBar1.Location = new Point(585, 165);
+            trackBar1.Location = new Point(562, 33);
             trackBar1.Maximum = 20;
             trackBar1.Name = "trackBar1";
             trackBar1.Size = new Size(203, 45);
@@ -164,26 +187,201 @@
             // 
             // trackBar2
             // 
-            trackBar2.Location = new Point(12, 195);
+            trackBar2.Location = new Point(13, 37);
             trackBar2.Maximum = 50;
             trackBar2.Minimum = -70;
             trackBar2.Name = "trackBar2";
-            trackBar2.Size = new Size(776, 45);
+            trackBar2.Size = new Size(752, 45);
             trackBar2.TabIndex = 3;
             trackBar2.Scroll += trackBar2_Scroll;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Enabled = false;
+            label3.Location = new Point(232, 162);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Fan Speed:";
+            // 
+            // txtFanSpeed
+            // 
+            txtFanSpeed.AutoSize = true;
+            txtFanSpeed.Enabled = false;
+            txtFanSpeed.Location = new Point(302, 162);
+            txtFanSpeed.Name = "txtFanSpeed";
+            txtFanSpeed.Size = new Size(28, 15);
+            txtFanSpeed.TabIndex = 2;
+            txtFanSpeed.Text = "00.0";
+            txtFanSpeed.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Enabled = false;
+            label7.Location = new Point(332, 162);
+            label7.Name = "label7";
+            label7.Size = new Size(32, 15);
+            label7.TabIndex = 2;
+            label7.Text = "RPM";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.ForeColor = Color.Gray;
+            label8.Location = new Point(13, 19);
+            label8.Name = "label8";
+            label8.Size = new Size(24, 15);
+            label8.TabIndex = 2;
+            label8.Text = "-70";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.ForeColor = Color.Gray;
+            label9.Location = new Point(746, 19);
+            label9.Name = "label9";
+            label9.Size = new Size(19, 15);
+            label9.TabIndex = 2;
+            label9.Text = "50";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.ForeColor = Color.Gray;
+            label10.Location = new Point(442, 19);
+            label10.Name = "label10";
+            label10.Size = new Size(13, 15);
+            label10.TabIndex = 2;
+            label10.Text = "0";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.ForeColor = Color.Gray;
+            label11.Location = new Point(600, 19);
+            label11.Name = "label11";
+            label11.Size = new Size(19, 15);
+            label11.TabIndex = 2;
+            label11.Text = "25";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.ForeColor = Color.Gray;
+            label12.Location = new Point(290, 19);
+            label12.Name = "label12";
+            label12.Size = new Size(24, 15);
+            label12.TabIndex = 2;
+            label12.Text = "-25";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label12);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(trackBar2);
+            groupBox1.Location = new Point(10, 285);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(776, 86);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Temprature Detecting Range";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(chckPlaySfx);
+            groupBox2.Controls.Add(trackBar1);
+            groupBox2.Controls.Add(label14);
+            groupBox2.Controls.Add(label13);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Location = new Point(10, 195);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(776, 84);
+            groupBox2.TabIndex = 5;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Basic Settings";
+            // 
+            // chckPlaySfx
+            // 
+            chckPlaySfx.AutoSize = true;
+            chckPlaySfx.Location = new Point(13, 22);
+            chckPlaySfx.Name = "chckPlaySfx";
+            chckPlaySfx.Size = new Size(70, 19);
+            chckPlaySfx.TabIndex = 4;
+            chckPlaySfx.Text = "Play SFX";
+            chckPlaySfx.UseVisualStyleBackColor = true;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.ForeColor = Color.Gray;
+            label14.Location = new Point(724, 15);
+            label14.Name = "label14";
+            label14.Size = new Size(46, 15);
+            label14.TabIndex = 2;
+            label14.Text = "slowest";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.ForeColor = Color.Gray;
+            label13.Location = new Point(562, 15);
+            label13.Name = "label13";
+            label13.Size = new Size(41, 15);
+            label13.TabIndex = 2;
+            label13.Text = "fastest";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(454, 33);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Update interval:";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Enabled = false;
+            label15.Location = new Point(213, 177);
+            label15.Name = "label15";
+            label15.Size = new Size(82, 15);
+            label15.TabIndex = 2;
+            label15.Text = "Battery Status:";
+            // 
+            // lblBatteryStatus
+            // 
+            lblBatteryStatus.AutoSize = true;
+            lblBatteryStatus.Enabled = false;
+            lblBatteryStatus.Location = new Point(302, 177);
+            lblBatteryStatus.Name = "lblBatteryStatus";
+            lblBatteryStatus.Size = new Size(56, 15);
+            lblBatteryStatus.TabIndex = 2;
+            lblBatteryStatus.Text = "Charging";
+            lblBatteryStatus.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(trackBar2);
-            Controls.Add(trackBar1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Controls.Add(label2);
             Controls.Add(label5);
             Controls.Add(txtCpuTemp);
             Controls.Add(label6);
+            Controls.Add(lblBatteryStatus);
+            Controls.Add(txtFanSpeed);
             Controls.Add(txtCpuLoad);
+            Controls.Add(label7);
+            Controls.Add(label15);
+            Controls.Add(label3);
             Controls.Add(label1);
             Controls.Add(chart1);
             Controls.Add(tempProgressBar);
@@ -193,6 +391,10 @@
             ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
             ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
             ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -209,5 +411,21 @@
         private Label label6;
         private TrackBar trackBar1;
         private TrackBar trackBar2;
+        private Label label3;
+        private Label txtFanSpeed;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private CheckBox chckPlaySfx;
+        private Label label14;
+        private Label label13;
+        private Label label4;
+        private Label label15;
+        private Label lblBatteryStatus;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
 using LibreHardwareMonitor.Hardware;
+using LibreHardwareMonitor.Hardware.Cpu;
+using System.Threading;
 using System.Windows.Forms.DataVisualization.Charting;
 using LOHM = LibreHardwareMonitor.Hardware;
 
@@ -13,14 +15,27 @@ namespace HeatChecker
         private static string SfxDir = Application.StartupPath + @"SFX\";
         private static string SfxBruh = SfxDir + "bruh.wav";
         private static string SfxVine = SfxDir + "vine.wav";
+        private static string SfxExit = SfxDir + "exit.wav";
+        private static string SfxPull = SfxDir + "pull.wav";
 
         private static string SfxAlarmS = SfxDir + @"alarms\alarm - slow.wav";
         private static string SfxAlarmM = SfxDir + @"alarms\alarm - medium.wav";
         private static string SfxAlarmF = SfxDir + @"alarms\alarm - fast.wav";
         private static string SfxAlarmC = SfxDir + @"alarms\alarm - critical.wav";
 
+        System.Media.SoundPlayer sound1 = new System.Media.SoundPlayer(SfxAlarmC);
+        System.Media.SoundPlayer sound2 = new System.Media.SoundPlayer(SfxAlarmS);
+        System.Media.SoundPlayer sound3 = new System.Media.SoundPlayer(SfxAlarmF);
+
+        System.Media.SoundPlayer lowBatterySound = new System.Media.SoundPlayer(SfxPull);
+
         private int M_GLOBAL_INT = 0;
         private int M_INCRES_INT = 0;
+
+        bool isPlayingBatterySound = false;
+        int batterySoundCounter = 0;
+
+        bool PlaySounds = true;
 
         public Form1()
         {
@@ -33,7 +48,16 @@ namespace HeatChecker
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            new System.Media.SoundPlayer(SfxVine).Play();
+            PlaySfx(SfxVine);
+
+            var _cancellationTokenSource = new CancellationTokenSource();
+            Task.Run(() => MonitorBatteryStatus(_cancellationTokenSource.Token));
+        }
+
+        private void PlaySfx(string SfxPath)
+        {
+            if (PlaySounds)
+                new System.Media.SoundPlayer(SfxPath).Play();
         }
 
         private void InitializeHardwareMonitor()
@@ -43,7 +67,13 @@ namespace HeatChecker
             {
                 IsCpuEnabled = true,
                 IsMemoryEnabled = true,
-                IsControllerEnabled = true
+                IsControllerEnabled = true,
+                IsMotherboardEnabled = true,
+                IsBatteryEnabled = true,
+                IsGpuEnabled = true,
+                IsNetworkEnabled = true,
+                IsStorageEnabled = true,
+                IsPsuEnabled = true,
             };
 
             // Start hardware monitoring
@@ -69,9 +99,11 @@ namespace HeatChecker
             chart1.Series.Add("CpuLoad");
             chart1.Series.Add("FanSpeed");
             */
+            chart1.ChartAreas[0].AxisX.IsStartedFromZero = true;
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;   // Remove extra margins for smooth scrolling
 
             chart1.Series["CpuTemp"].ChartType = SeriesChartType.Line;
-            chart1.Series["CpuLoad"].ChartType = SeriesChartType.Line;
+            chart1.Series["CpuLoad"].ChartType = SeriesChartType.Spline;
             chart1.Series["FanSpeed"].ChartType = SeriesChartType.Spline;
 
             chart1.ChartAreas[0].AxisX.Title = "Time";
@@ -91,6 +123,7 @@ namespace HeatChecker
 
         private void Timer_Tick(object sender, EventArgs e)
         {
+            float cpuTempUI = 0;
             foreach (var hardwareItem in _computer.Hardware)
             {
                 if (hardwareItem.HardwareType == HardwareType.Cpu)
@@ -100,7 +133,13 @@ namespace HeatChecker
                     {
                         if (sensor.SensorType == SensorType.Temperature && sensor.Name == "Core Average")
                         {
+                            if (M_GLOBAL_INT == 1)
+                            {
+                                chart1.Series["CpuTemp"].Points.AddXY(0, 100);
+
+                            }
                             float temp = sensor.Value.GetValueOrDefault() + M_INCRES_INT;
+                            cpuTempUI = sensor.Value.GetValueOrDefault();
                             if (temp < 0)
                                 tempProgressBar.Value = 0;
                             else if (temp > 100)
@@ -110,12 +149,17 @@ namespace HeatChecker
 
                             chart1.Series["CpuTemp"].Points.AddXY(M_GLOBAL_INT, temp); // Update chart
                             if (chart1.Series["CpuTemp"].Points.Count > 100)
-                                chart1.Series["CpuTemp"].Points.RemoveAt(0);
+                            {
+                                chart1.Series["CpuTemp"].Points.Clear();
+                                M_GLOBAL_INT = 0;
+                            }
 
-                            txtCpuTemp.Text = temp.ToString();
+                            txtCpuTemp.Text = cpuTempUI.ToString();
+
+                            ScrollChartLeft("CpuTemp");
 
                             var category = CategoryBasedOnHeat(temp);
-                            if (category != HeatCategory.Normal)
+                            if (M_GLOBAL_INT != 0)// M_GLOBAL_INT is only zero at the first time the program starts and first data extracted!
                             {
                                 HandleCpuTemprature(category);
                                 continue;
@@ -129,10 +173,14 @@ namespace HeatChecker
                     {
                         if (sensor.SensorType == SensorType.Load && sensor.Name == "CPU Total")
                         {
-                            chart1.Series["CpuLoad"].Points.AddXY(M_GLOBAL_INT, sensor.Value.GetValueOrDefault());
+                            chart1.Series["CpuLoad"].Points.AddXY(M_GLOBAL_INT, sensor.Value.GetValueOrDefault() > 100 ? 100 : sensor.Value);
                             if (chart1.Series["CpuLoad"].Points.Count > 100)
-                                chart1.Series["CpuLoad"].Points.RemoveAt(0);
+                            {
+                                chart1.Series["CpuLoad"].Points.Clear();
+                                M_GLOBAL_INT = 0;
+                            }
                             txtCpuLoad.Text = sensor.Value.ToString();
+                            ScrollChartLeft("CpuLoad");
                         }
                     }
                     /*
@@ -151,6 +199,52 @@ namespace HeatChecker
                     M_GLOBAL_INT++;
                 }
             }
+
+
+
+        }
+
+        private async Task MonitorBatteryStatus(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                PowerStatus pwr = System.Windows.Forms.SystemInformation.PowerStatus;
+
+                bool isCharging = pwr.PowerLineStatus == PowerLineStatus.Online ? true : false;
+
+                var chargeStatus = pwr.BatteryChargeStatus;
+
+                string chrgl = chargeStatus.ToString().ToLower();
+
+                if (!isCharging || (chrgl.Contains("low") && !isCharging))
+                {
+                    HandleLowBattery();
+                }
+            }
+        }
+
+        private void HandleLowBattery()
+        {
+                lowBatterySound.PlaySync();
+        }
+
+        private void ScrollChartLeft(string seriesName)
+        {
+            var series = chart1.Series[seriesName];
+            // Ensure X-axis range scrolls dynamically
+            const int visiblePoints = 35; // Adjust this to your desired range
+            if (series.Points.Count > visiblePoints)
+            {
+                // Keep updating the X-axis range
+                chart1.ChartAreas[0].AxisX.Minimum = series.Points.Count - visiblePoints;
+                chart1.ChartAreas[0].AxisX.Maximum = series.Points.Count;
+            }
+            else
+            {
+                // Set initial range for fewer points
+                chart1.ChartAreas[0].AxisX.Minimum = 0;
+                chart1.ChartAreas[0].AxisX.Maximum = visiblePoints;
+            }
         }
 
         private void HandleCpuTemprature(HeatCategory category)
@@ -162,27 +256,40 @@ namespace HeatChecker
                 case HeatCategory.none:
                     txtCpuTemp.Text = "??.?";
                     txtCpuTemp.Enabled = false;
+                    StopSounds();
                     break;
                 case HeatCategory.Freezing:
                     txtCpuTemp.ForeColor = Color.DeepSkyBlue;
+                    sound1.Play();
                     break;
                 case HeatCategory.Normal:
-
+                    StopSounds();
                     txtCpuTemp.Enabled = false;
                     break;
                 case HeatCategory.Warm:
                     txtCpuTemp.Enabled = false;
+                    StopSounds();
                     break;
                 case HeatCategory.Hot:
                     txtCpuTemp.ForeColor = Color.Red;
+                    sound2.Play();
                     break;
                 case HeatCategory.Critical:
                     txtCpuTemp.ForeColor = Color.DarkRed;
+                    sound3.Play();
                     break;
                 default:
                     txtCpuTemp.Enabled = false;
+                    StopSounds();
                     break;
             }
+        }
+
+        private void StopSounds()
+        {
+            sound1.Stop();
+            sound2.Stop();
+            sound3.Stop();
         }
 
         private HeatCategory CategoryBasedOnHeat(float? heat)
@@ -202,6 +309,7 @@ namespace HeatChecker
 
         protected override void OnFormClosed(FormClosedEventArgs e)
         {
+            PlaySfx(SfxExit);
             _computer.Close();
             base.OnFormClosed(e);
         }
